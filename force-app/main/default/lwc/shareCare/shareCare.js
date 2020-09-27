3767c93462bdb46ac7af1bd9656b151e5fa774f1
Reid Efford <reidefford@gmail.com>
2020-09-26 20:31:33 -0400

    group query and grouped-uids...

--- force-app/main/default/lwc/shareCare/shareCare.js
+++ force-app/main/default/lwc/shareCare/shareCare.js
@@ -1,5 +1,7 @@
 import { LightningElement, api, track } from 'lwc';
 import getUsers from '@salesforce/apex/ShareCareHelper.getUsers';
+import getGroupedUsers from '@salesforce/apex/contractorSharingHelper.getGroupedUsers'
+import getGroups from '@salesforce/apex/ShareCareHelper.getGroups';
 
 import { userTable, groupTable } from './tableData'
 
@@ -12,12 +14,36 @@ export default class ShareCare extends LightningElement {
     @track groups = []
     @track groupedUsers = []
 
+    @track _selectedGroups
+
     @track userColumns = userTable()
     @track groupColumns = groupTable()
 
     async connectedCallback() {
 
         this.users = await getUsers({ p:this.profile })
+        this.groups = await getGroups()
+    }
 
+    //user table functions
+    async fetchGroupedUsers() {
+
+        const result = await getGroupedUsers({ groupIds: this._selectedGroups })
+
+        this.groupedUsers = [...result]
+
+    }
+
+    //group table functions
+    getSelectedGroups(event) {
+        const selected = event.detail.selectedRows
+
+        for (let s of selected) {
+            this._selectedGroups.push(s.Id)
+        } 
+
+        if (this._selectedGroups.length) {
+            //fetch the users
+        }
     }
 }
